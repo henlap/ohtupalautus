0829b476c9e68d5c43f34c164d7edbb641434408
henlap <144265154+henlap@users.noreply.github.com>
2024-11-11 18:42:09 +0200

    11

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,36 @@
+import requests
 from player import Player
+from enum import Enum
+
+class SortBy(Enum):
+    POINTS = 1
+    GOALS = 2
+    ASSISTS = 3
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
+    response = requests.get(url).json()
+
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
+    
+    players = []
+
+    for player_dict in response:
+        player = Player(player_dict)
+        players.append(player)
+
+    sorted_players = sorted(
+            players,
+            reverse=True,
+            key= lambda p: (p.points, p.goals)
+        )
+
+    print("Players from FIN:\n")
+    
+    for player in sorted_players:
+        if player.nation == "FIN":
+            print(player)
 
 if __name__ == "__main__":
     main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,14 @@
 class Player:
     def __init__(self, dict):
         self.name = dict['name']
+        self.team = dict['team']
+        self.nation = dict['nationality']
+        self.goals = dict['goals']
+        self.assists = dict['assists']
+    @property
+    def points(self):
+        return self.goals + self.assists
+
     
     def __str__(self):
-        return self.name
+        return f"{self.name:20} {self.team:4} {self.goals:2} + {self.assists:2} = {self.goals + self.assists}"
